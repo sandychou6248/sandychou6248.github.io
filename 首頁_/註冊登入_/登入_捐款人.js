8a2699ee6f1eb96b956399b7aec273be81d9b3f0
sandychou6248 <68326531+sandychou6248@users.noreply.github.com>
2021-05-21 19:38:22 +0800

    Update 登入_捐款人.js

--- 首頁_/註冊登入_/登入_捐款人.js
+++ 首頁_/註冊登入_/登入_捐款人.js
@@ -8,6 +8,9 @@ $("#donate").click(function () {
       for(var i=0;i<fundraiser.length;i++){
          if($("#fundraiser").val() == fundraiser[i]){		
             var result=infoContract.methods.donate($("#fundraiser").val()).send({ from:$.query.get("address"), value: ($("#value").val())*Math.pow(10,18), gas: 5000000 });
+            result.catch(function(error){
+					alert("匯款失敗!" + JSON.stringify(error));
+				})
             result.then(function(){
                alert("捐款成功!");
             })					
